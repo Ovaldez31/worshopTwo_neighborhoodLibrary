59ef9835f76060bb57af6b598dd9a319e88ae9f6
Orchid <ovaldez@appdev.year.org>
2025-04-18 12:05:01 -0400

    first commit

---
+++ src/main/java/com/ps/Book.java
@@ -0,0 +1,11 @@
+package com.ps;
+
+import java.util.Scanner;
+
+public class Book {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int checkedOut
+    }
+}
